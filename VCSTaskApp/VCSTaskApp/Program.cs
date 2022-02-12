067da4d7a81cf28de72e8c354fed74d7a23a49fd
rodionuser12345 <79834564+rodionuser12345@users.noreply.github.com>
2022-02-12 16:15:24 +0200

    Make valid namespace name

--- VCSTaskApp/VCSTaskApp/Program.cs
+++ VCSTaskApp/VCSTaskApp/Program.cs
@@ -1,9 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Threading.Channels;
-using System;
-
-namespace basic {
+namespace VCSTaskApp {
 
     class VCSApp {
 
