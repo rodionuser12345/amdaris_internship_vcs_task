3d34679c52452a0f78df95b8762b3716c74fa683
rodionuser12345 <79834564+rodionuser12345@users.noreply.github.com>
2022-02-12 15:44:50 +0200

    Add Application

---
+++ VCSTaskApp/VCSTaskApp/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+using System;
+
+namespace basic {
+
+    class VCSApp {
+
+        public static void Main(string[] args) {
+            Console.WriteLine(Area(2));
+            Console.WriteLine(Area(3.02));
+            Console.WriteLine(Area(2.2, 3.3));
+
+        }
+
+        public static string Area(int value1) {
+            return String.Format("Area of Square is {0}", value1 * value1);
+        }
+
+        public static string Area(double value1) {
+            return String.Format("Area of Circle is {0}", 3.14 * Math.Pow(value1, 2));
+        }
+
+        public static string Area(double value1, double value2) {
+            return String.Format("Area of Rectangle is {0}", value1 * value2);
+        }
+    }
+
+}
